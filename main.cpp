618fe29d849dba14474d5507f57880ae6b3479c5
VladLevotskiy <vlad.levotskiy@gmail.com>
2017-06-28 17:39:21 +0300

    обучение с случайным порядком входных данных

--- main.cpp
+++ main.cpp
@@ -23,23 +23,6 @@ int main(int argc, char *argv[])
                       {1.0, 1.0, 1.0}};
 
     training("C:/Users/coder/Documents/build-ConvolutionNeuralNetwork-Desktop_Qt_5_5_1_MinGW_32bit-Release/release/",new_net);
-    /*std::vector<double> inp;
-    std::vector<double> out;
-    out.push_back(1);
-    inp.push_back(0);
-    inp.push_back(1);
-    int cntr =0;
-    for (int i =0; i < 10; i++){
-        inp[0] = mat[cntr][0];
-        inp[1] = mat[cntr][1];
-        new_net.forward_propagation(inp);
-        out[0] = mat[cntr][2];
-        std::cout << "out = " << out[0] << std::endl;
-        new_net.back_propagation(out);
-        cntr++;
-        if (cntr ==  4) cntr = 0;
-    }
-    std::cout << "CREATED!" << std::endl;*/
 
     return a.exec();
 }
@@ -55,8 +38,8 @@ void training(const std::string& path, NeuralNet& nn){
         for (;i<200;i++){
             for (int j =0;j < 21; j++){
 
-                //std::string new_path =path + "13_21/";
-                std::string new_path =path + "7_13/";
+                std::string new_path =path + "13_21/";
+                //std::string new_path =path + "7_13/";
                 switch(j){
                 case 0:{new_path.append("0/"); break;}
                 case 1:{new_path.append("1/"); break;}
@@ -90,7 +73,7 @@ void training(const std::string& path, NeuralNet& nn){
 
                 char y;
                 int symbls_cntr = 0;
-                int img_width = 7;
+                int img_width = 13;
                 int padding = img_width +(img_width*3)%4;
                 while (!input.eof())
                 {
@@ -121,45 +104,132 @@ void training(const std::string& path, NeuralNet& nn){
                 }
                 actual_value[j] = 0;
 
+            }
+        }
+    //}
+}
+
+void training2(){
+    std::vector<double> actual_value;
+    for (size_t i = 0; i < 21; i++){
+        actual_value.push_back(0);
+    }
+
+    uint16_t random[501];
+    bool randomed[501];
+
+    //for (size_t k =0; k < epochs; k++){
+        memset(&randomed,0,501*sizeof(bool));
+        memset(&random,0,501*sizeof(uint16_t));
+        for (int r = 0; r < 500; r++){
+            bool next = false;
+
+            while (!next){
+                int newr = rand()%500;
+                if (randomed[newr] == false){
+                    random[r] = newr;
+                    randomed[newr] = true;
+                    next = true;
+                }
+            }
+        }
 
 
-                /*new_path2 += std::to_string(i)+".bmp";
+        int i = 1;
+        for (;i<500;i++){
+            for (int j =0;j < 21; j++){
+
+                std::string new_path =path + "13_21/";
+                //std::string new_path =path + "7_13/";
+                switch(j){
+                case 0:{new_path.append("0/"); break;}
+                case 1:{new_path.append("1/"); break;}
+                case 2:{new_path.append("2/"); break;}
+                case 3:{new_path.append("3/"); break;}
+                case 4:{new_path.append("4/"); break;}
+                case 5:{new_path.append("5/"); break;}
+                case 6:{new_path.append("6/"); break;}
+                case 7:{new_path.append("7/"); break;}
+                case 8:{new_path.append("8/"); break;}
+                case 9:{new_path.append("9/"); break;}
+                case 10:{new_path.append("A/"); break;}
+                case 11:{new_path.append("B/"); break;}
+                case 12:{new_path.append("C/"); break;}
+                case 13:{new_path.append("E/"); break;}
+                case 14:{new_path.append("H/"); break;}
+                case 15:{new_path.append("K/"); break;}
+                case 16:{new_path.append("M/"); break;}
+                case 17:{new_path.append("P/"); break;}
+                case 18:{new_path.append("T/"); break;}
+                case 19:{new_path.append("X/"); break;}
+                case 20:{new_path.append("Y/"); break;}
+                default:{continue;break;}
+                }
+                new_path += std::to_string(random[i])+".bmp";
                 //std::cout << new_path << std::endl;
-                input_vector.clear();
-                std::ifstream input2(new_path, std::ios::binary);
-                if (!input2.is_open()) throw std::runtime_error("Can't open file " + new_path);
-                input2.seekg(1078);
+                std::vector<double> input_vector;
+                std::ifstream input(new_path, std::ios::binary);
+                if (!input.is_open()) throw std::runtime_error("Can't open file " + new_path);
+                input.seekg(1078);
 
-                symbls_cntr = 0;
-                img_width = 7;
-                padding = img_width +(img_width*3)%4;
-                while (!input2.eof())
+                char y;
+                int symbls_cntr = 0;
+                int img_width = 13;
+                int padding = img_width +(img_width*3)%4;
+                while (!input.eof())
                 {
-                    input2.get(y);
+                    input.get(y);
                     symbls_cntr++;
                     if (symbls_cntr < img_width){
                         input_vector.push_back(y /255.);
                     }
                     if (symbls_cntr == padding-1) symbls_cntr = 0;
                 }
-                input2.close();
-                begin = std::chrono::high_resolution_clock::now();
+                input.close();
+                auto begin = std::chrono::high_resolution_clock::now();
 
                 nn.forward_propagation(input_vector);
 
-                end = std::chrono::high_resolution_clock::now();
+                auto end = std::chrono::high_resolution_clock::now();
                 std::cout<< std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count()<<"ms"<< std::endl;
 
+                nn.Get_last_layer();
+
                 actual_value[j] = 1;
-                if (i < 180){
+               // if (i < 180){
                     nn.back_propagation(actual_value);
-                }
+                /*}
                 else {
                     std::cout << "symbol " << j << std::endl;
                     nn.Get_last_layer();
                 }
                 actual_value[j] = 0;*/
+
             }
         }
-    //}
+
+    /*int errors_cntr = 0;
+    for (int i = 0;i < 50;i++){
+        for(int j =0; j < 21; j++){
+        //int l = rand()%250;
+        int num = i;
+        float inp[input_size];
+        for (int m = 0; m < height;m++){
+            for (size_t n = 0; n < width; n++){
+                inp [n+m*width]= ((unsigned char)array[j][random[i]][(m*(width+3))+n+1078])/255.;
+            }
+        }
+
+        out = xor_net->forward_propagation(inp,input_size);
+        out_neuron = out->GetNeurons();
+        float max = 0.0;
+        unsigned int num_max = 0;
+        for (int j =0; j < out->Size()-1;j++){
+            if (out_neuron[j].Out_value() > max) {num_max = j; max =out_neuron[j].Out_value();}
+        }
+        if (j!=num_max) errors_cntr++;
+            std::cout << "num " << j << " result: " << num_max << " = " << out_neuron[num_max].Out_value() << std::endl;
+        }
+    }*/
+
 }
